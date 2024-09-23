df1b40f193fab598c58993b074d2639abfaf3d77
Beatrix Eysseric <beatrix.eysseric@epfedu.fr>
2024-09-23 17:02:37 +0200

    Creation d'un nouveay projet versionné

--- Exo2/src/exo2/Exo2.java
+++ Exo2/src/exo2/Exo2.java
@@ -17,9 +17,8 @@ public class Exo2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    public static void main(String[] args) {
-//Declaration des variables
+        
+    //Declaration des variables
 int nb; // nombre dentiers a additionner
 int result; // resultat
 int ind; //indice
@@ -29,9 +28,10 @@ result=0;
 ind=1;
 while (ind <= nb) {
 result=result+ind;
-}
+ind++}
 // Affichage du resultat
 System.out.println();
-System.out.println("La somme des "+ nb + "entiers est: "+result);
+System.out.println("La somme des "+ nb + "entiers est: "+result);}
+
     
 }
