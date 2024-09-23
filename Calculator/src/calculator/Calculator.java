3e8481a8a6fcc1de8b0621e62814669ef2ed5a93
Beatrix Eysseric <beatrix.eysseric@epfedu.fr>
2024-09-23 17:33:23 +0200

    Creation d'un nouveay projet versionné

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -1,4 +1,6 @@
-/*
+/*TP0 Exercice 1
+ * Eysseric Béatrix
+ * 23/09/2024
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
@@ -15,6 +17,13 @@ public class Calculator {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("1) add");
+        System.out.println("2) substract");
+        System.out.println("3) multiply");
+        System.out.println("4) divide");
+        System.out.println("5) modulo");
+        
+        int valeur
     }
     
 }
