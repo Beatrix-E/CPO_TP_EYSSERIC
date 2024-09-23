f79c8bdd7ef9b4e3a2739226d84e15f85a3dd2ff
Beatrix Eysseric <beatrix.eysseric@epfedu.fr>
2024-09-23 18:24:56 +0200

    Creation d'un nouveay projet versionné

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -10,6 +10,7 @@ package calculator;
  *
  * @author beatr
  */
+import java.util.Scanner;
 public class Calculator {
 
     /**
@@ -23,7 +24,73 @@ public class Calculator {
         System.out.println("4) divide");
         System.out.println("5) modulo");
         
-        int valeur
+        
+      
+        Scanner scanner = new Scanner(System.in); // Crée un objet Scanner pour lire l'entrée de l'utilisateur
+
+        System.out.print("Veuillez saisir un nombre entier : "); // Demande à l'utilisateur de saisir un nombre
+        int operateur = scanner.nextInt(); // Récupère la valeur entière saisie et la stocke dans la variable operateur
+
+        System.out.println("Vous avez saisi : " + operateur); // Affiche la valeur saisie
+        scanner.close(); // Ferme le scanner
+        
+        
+        
+        // Afficher un message pour demander à l'utilisateur une première valeur
+        System.out.print("Veuillez entrer la première valeur : ");
+        
+        // Récupérer la saisie de l'utilisateur dans la variable operande1
+        double operande1 = scanner.nextDouble();
+        
+
+        
+        // Afficher la valeur saisie (facultatif)
+        System.out.println("Vous avez saisi : " + operande1);
+        
+   
+        
+        
+        
+        // Afficher un message pour demander à l'utilisateur une première valeur
+        System.out.print("Veuillez entrer la deusieme valeur : ");
+        
+        // Récupérer la saisie de l'utilisateur dans la variable operande1
+        double operande2 = scanner.nextDouble();
+        
+      
+        
+        // Afficher la valeur saisie (facultatif)
+        System.out.println("Vous avez saisi : " + operande2);
+        double resultat = 0;
+        
+            
+        if (operateur==1) {
+            resultat = operande1 + operande2;
+                System.out.println("Le resultat de l'addition est:" + resultat);
+        } else if (operateur==2) {
+                resultat = operande1 - operande2;
+                System.out.println("Le resultat de la soustraction est:" + resultat);
+        } else if (operateur==3) {
+                resultat = operande1 * operande2;
+                System.out.println("Le resultat de la multiplication est:" + resultat);
+        } else if (operateur==4) {
+                if (operande2 != 0) {
+                System.out.println("Le resultat de la soustraction est:" + resultat);
+                    resultat = operande1 / operande2;
+                } else {
+                    System.out.println("Erreur: Division par zero n'est pas autorisé.");
+                }
+        } else if (operateur==5) {
+                resultat = operande1 % operande2;
+                System.out.println("Le resultat du modulo est:" + resultat);
+            } else {
+                System.out.println("operateur invalide.");
+                
+        }
+        
+        System.out.println("The result is: " + resultat);
+        }
     }
-    
-}
+
+        
+   
