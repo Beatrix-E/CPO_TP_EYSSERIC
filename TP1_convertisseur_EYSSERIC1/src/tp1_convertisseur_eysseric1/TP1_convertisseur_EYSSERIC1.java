2f049d69b6c27530d7cf08696dd8158b51f3f1ab
Beatrix Eysseric <beatrix.eysseric@epfedu.fr>
2024-10-07 19:55:41 +0200

    Creation d'un nouveay projet versionné

--- TP1_convertisseur_EYSSERIC1/src/tp1_convertisseur_eysseric1/TP1_convertisseur_EYSSERIC1.java
+++ TP1_convertisseur_EYSSERIC1/src/tp1_convertisseur_eysseric1/TP1_convertisseur_EYSSERIC1.java
@@ -16,51 +16,78 @@ public class TP1_convertisseur_EYSSERIC1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Création d'un objet Scanner pour lire l'entrée utilisateur
         Scanner scanner = new Scanner(System.in);
         
-        // Demande à l'utilisateur de saisir une valeur réelle
-        System.out.print("Veuillez saisir une valeur réelle : ");
-        double valeurReelle = scanner.nextDouble();
+        // Demande de saisie d'une valeur
+        System.out.print("Bonjour, saisissez une valeur : ");
+        double temperature = scanner.nextDouble();
         
-        // Affichage de la valeur saisie
-        System.out.println("Vous avez saisi : " + valeurReelle);
+        // Affichage du menu de conversion
+        System.out.println("Saisissez la conversion que vous souhaitez effectuer :");
+        System.out.println("1) De Celsius vers Kelvin");
+        System.out.println("2) De Kelvin vers Celsius");
+        System.out.println("3) De Celsius vers Fahrenheit");
+        System.out.println("4) De Fahrenheit vers Celsius");
+        System.out.println("5) De Kelvin vers Fahrenheit");
+        System.out.println("6) De Fahrenheit vers Kelvin");
         
-        // Demande à l'utilisateur de saisir une température en degrés Celsius
-        System.out.print("Veuillez saisir une température en degrés Celsius : ");
-        double temperatureCelsius = scanner.nextDouble();
+        // Saisie du choix de l'utilisateur
+        int choix = scanner.nextInt();
         
-        // Conversion de Celsius à Kelvin
-        double temperatureKelvin = temperatureCelsius + 273;
-        
-        // Affichage des résultats
-        System.out.println("La température en degrés Kelvin est : " + temperatureKelvin);
-        
+        switch (choix) {
+            case 1:
+                System.out.println(temperature + " degrés Celsius est égal à " + CelciusVersKelvin(temperature) + " degrés Kelvin");
+                break;
+            case 2:
+                System.out.println(temperature + " degrés Kelvin est égal à " + KelvinVersCelcius(temperature) + " degrés Celsius");
+                break;
+            case 3:
+                System.out.println(temperature + " degrés Celsius est égal à " + CelciusVersFahrenheit(temperature) + " degrés Fahrenheit");
+                break;
+            case 4:
+                System.out.println(temperature + " degrés Fahrenheit est égal à " + FahrenheitVersCelcius(temperature) + " degrés Celsius");
+                break;
+            case 5:
+                System.out.println(temperature + " degrés Kelvin est égal à " + KelvinVersFahrenheit(temperature) + " degrés Fahrenheit");
+                break;
+            case 6:
+                System.out.println(temperature + " degrés Fahrenheit est égal à " + FahrenheitVersKelvin(temperature) + " degrés Kelvin");
+                break;
+            default:
+                System.out.println("Choix invalide.");
+                break;
+        }
     }
+
     // Méthode pour convertir Celsius en Kelvin
-    public static double CelciusVersKelvin(double celsius) {
-        return celsius + 273; // Formule de conversion
-    }
-    
-    public static double KelvinVersCelcius(double kelvin) {
-        return kelvin - 273; // Conversion de Kelvin à Celsius
+    public static double CelciusVersKelvin(double tCelcius) {
+        return tCelcius + 273.15; // Formule de conversion
     }
 
-    public static double FarenheitVersCelcius(double fahrenheit) {
-        return (fahrenheit - 32) * 5 / 9; // Conversion de Fahrenheit à Celsius
+    // Méthode pour convertir Kelvin en Celsius
+    public static double KelvinVersCelcius(double tKelvin) {
+        return tKelvin - 273.15; // Formule de conversion
     }
 
-    public static double CelciusVersFarenheit(double celsius) {
-        return (celsius * 9 / 5) + 32; // Conversion de Celsius à Fahrenheit
+    // Méthode pour convertir Celsius en Fahrenheit
+    public static double CelciusVersFahrenheit(double tCelcius) {
+        return (tCelcius * 9/5) + 32; // Formule de conversion
     }
 
-    public static double KelvinVersFarenheit(double kelvin) {
-        return CelciusVersFarenheit(KelvinVersCelcius(kelvin)); // Conversion de Kelvin à Fahrenheit
+    // Méthode pour convertir Fahrenheit en Celsius
+    public static double FahrenheitVersCelcius(double tFahrenheit) {
+        return (tFahrenheit - 32) * 5/9; // Formule de conversion
     }
 
-    public static double FarenheitVersKelvin(double fahrenheit) {
-        return CelciusVersKelvin(FarenheitVersCelcius(fahrenheit)); // Conversion de Fahrenheit à Kelvin
+    // Méthode pour convertir Kelvin en Fahrenheit
+    public static double KelvinVersFahrenheit(double tKelvin) {
+        double tCelcius = KelvinVersCelcius(tKelvin); // Conversion de Kelvin à Celsius
+        return CelciusVersFahrenheit(tCelcius); // Conversion de Celsius à Fahrenheit
     }
-    
-    
+
+    // Méthode pour convertir Fahrenheit en Kelvin
+    public static double FahrenheitVersKelvin(double tFahrenheit) {
+        double tCelcius = FahrenheitVersCelcius(tFahrenheit); // Conversion de Fahrenheit à Celsius
+        return CelciusVersKelvin(tCelcius); // Conversion de Celsius à Kelvin
+    }
 }
