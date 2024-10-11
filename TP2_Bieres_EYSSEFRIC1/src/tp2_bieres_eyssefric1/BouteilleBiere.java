5e29c5c5222fda207c16ae2b3c1e2f1016899943
Beatrix Eysseric <beatrix.eysseric@epfedu.fr>
2024-10-11 10:15:31 +0200

    Creation d'un nouveay projet versionné

---
+++ TP2_Bieres_EYSSEFRIC1/src/tp2_bieres_eyssefric1/BouteilleBiere.java
@@ -0,0 +1,14 @@
+package tp2_bieres_eyssefric1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author beatr
+ */
+public class BouteilleBiere {
+    
+}
