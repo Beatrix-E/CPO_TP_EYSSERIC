eafdf9da446ce8884d40472fa573ce2e2f5ff3b5
Beatrix Eysseric <beatrix.eysseric@epfedu.fr>
2024-10-11 11:14:37 +0200

    Creation d'un nouveay projet versionné

--- TP2_Bieres_EYSSEFRIC1/src/tp2_bieres_eyssefric1/TP2_Bieres_EYSSEFRIC1.java
+++ TP2_Bieres_EYSSEFRIC1/src/tp2_bieres_eyssefric1/TP2_Bieres_EYSSEFRIC1.java
@@ -14,7 +14,32 @@ public class TP2_Bieres_EYSSEFRIC1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0,"Dubuisson") ;
+        uneBiere.lireEtiquette();
+        uneBiere.Décapsuler();
+        
+        BouteilleBiere secondeBiere = new BouteilleBiere("Leffe", 6.6,"Abbaye de Leffe") ;
+        secondeBiere.lireEtiquette();
+        secondeBiere.Décapsuler();
+        
+        BouteilleBiere troisiemeBiere = new BouteilleBiere("Desperados", 5.9,"Heineken N.V.") ;
+        troisiemeBiere.lireEtiquette();
+        troisiemeBiere.Décapsuler();
+        
+        BouteilleBiere quatriemeBiere = new BouteilleBiere("Heineken", 5.0,"Heineken N.V.") ;
+        quatriemeBiere.lireEtiquette();
+        quatriemeBiere.Décapsuler();
+        
+        BouteilleBiere cinquiemeBiere = new BouteilleBiere("Corona", 4.6,"Grupo Modelo") ;
+        cinquiemeBiere.lireEtiquette();
+        cinquiemeBiere.Décapsuler();
+        
+        System.out.println(uneBiere) ;
+        System.out.println(secondeBiere) ;
+        System.out.println(troisiemeBiere) ;
+        System.out.println(quatriemeBiere) ;
+        System.out.println(cinquiemeBiere) ;
+   
     }
     
 }
