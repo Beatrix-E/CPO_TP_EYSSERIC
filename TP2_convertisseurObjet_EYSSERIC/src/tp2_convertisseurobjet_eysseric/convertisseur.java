bd6fe2132ab829c022c7c6054c55d613d0654ead
Beatrix Eysseric <beatrix.eysseric@epfedu.fr>
2024-10-23 16:17:48 +0200

    Creation d'un nouveay projet versionné

--- TP2_convertisseurObjet_EYSSERIC/src/tp2_convertisseurobjet_eysseric/convertisseur.java
+++ TP2_convertisseurObjet_EYSSERIC/src/tp2_convertisseurobjet_eysseric/convertisseur.java
@@ -9,5 +9,55 @@ package tp2_convertisseurobjet_eysseric;
  * @author beatr
  */
 public class convertisseur {
+    int nbConversions;
+    public convertisseur() {
+        nbConversions = 0;
+    }
+
+ 
+        
+    
+
+    // Méthode pour convertir Celsius en Kelvin
+    public double CelciusVersKelvin(double tCelcius) {
+        nbConversions++;
+        return tCelcius + 273.15; // Formule de conversion
+    }
+     // Méthode pour convertir Kelvin en Celsius
+    public double KelvinVersCelcius(double tKelvin) {
+        nbConversions++;
+        return tKelvin - 273.15; // Formule de conversion
+    }
+         // Méthode pour convertir Celsius en Fahrenheit
+    public double CelciusVersFahrenheit(double tCelcius) {
+        nbConversions++;
+        return (tCelcius * 9/5) + 32; // Formule de conversion
+    }
+     // Méthode pour convertir Fahrenheit en Celsius
+    public double FahrenheitVersCelcius(double tFahrenheit) {
+        nbConversions++;
+        return (tFahrenheit - 32) * 5/9; // Formule de conversion
+    }
+        // Méthode pour convertir Kelvin en Fahrenheit
+    public double KelvinVersFahrenheit(double tKelvin) {
+        double tCelcius = KelvinVersCelcius(tKelvin); // Conversion de Kelvin à Celsius
+        nbConversions++;
+        return CelciusVersFahrenheit(tCelcius); // Conversion de Celsius à Fahrenheit
+    }
+        // Méthode pour convertir Fahrenheit en Kelvin
+    public double FahrenheitVersKelvin(double tFahrenheit) {
+        double tCelcius = FahrenheitVersCelcius(tFahrenheit); // Conversion de Fahrenheit à Celsius
+        nbConversions++;
+        return CelciusVersKelvin(tCelcius); // Conversion de Celsius à Kelvin
+    }
+        
+
+    @Override
+    public String toString () {
+        return "nb de conversions"+ nbConversions;
+    }  
+
     
 }
+
+
