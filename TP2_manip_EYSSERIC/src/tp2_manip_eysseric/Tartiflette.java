f831be3e3d15d7df7ab4944feed151ced4310b5c
Beatrix Eysseric <beatrix.eysseric@epfedu.fr>
2024-10-23 16:43:32 +0200

    Creation d'un nouveay projet versionné

---
+++ TP2_manip_EYSSERIC/src/tp2_manip_eysseric/Tartiflette.java
@@ -0,0 +1,19 @@
+package tp2_manip_eysseric;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author beatr
+ */
+public class Tartiflette {
+    int nbCalories;
+
+    public Tartiflette(int nbCalories) {
+        this.nbCalories = nbCalories;
+    }
+    
+}
