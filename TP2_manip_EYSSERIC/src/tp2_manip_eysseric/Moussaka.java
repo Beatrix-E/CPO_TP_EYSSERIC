d112c366ff102513013c61b2a0fcab9d5ddb303a
Beatrix Eysseric <beatrix.eysseric@epfedu.fr>
2024-10-23 16:43:16 +0200

    Creation d'un nouveay projet versionné

---
+++ TP2_manip_EYSSERIC/src/tp2_manip_eysseric/Moussaka.java
@@ -0,0 +1,19 @@
+package tp2_manip_eysseric;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author beatr
+ */
+public class Moussaka {
+    int nbCalories;
+
+    public Moussaka(int nbCalories) {
+        this.nbCalories = nbCalories;
+    }
+     
+}
