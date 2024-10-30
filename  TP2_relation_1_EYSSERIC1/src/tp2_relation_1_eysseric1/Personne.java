b83c5704f2b2e576cf268404028435af1da9cf3c
Beatrix Eysseric <beatrix.eysseric@epfedu.fr>
2024-10-30 16:53:34 +0100

    Creation d'un nouveay projet versionné

---  TP2_relation_1_EYSSERIC1/src/tp2_relation_1_eysseric1/Personne.java
+++  TP2_relation_1_EYSSERIC1/src/tp2_relation_1_eysseric1/Personne.java
@@ -9,20 +9,48 @@ package tp2_relation_1_eysseric1;
  * @author beatr
  */
 public class Personne {
-    String prenom;
     String nom;
+    String prenom;
+    int nbVoitures; // Nombre de voitures possédées
+    Voiture[] liste_voitures;
 
-    public Personne(String prenom, String nom) {
-        this.prenom = prenom;
+    // Constructeur
+    public Personne(String nom, String prenom) {
         this.nom = nom;
+        this.prenom = prenom;
+        this.liste_voitures = new Voiture[3]; // Réserve de la mémoire pour 3 voitures
+        this.nbVoitures = 0; // Initialisation du nombre de voitures possédées à 0
     }
 
+    // Méthode toString
     @Override
     public String toString() {
-        return "Personne{" + 
-                "prenom=" + prenom + ","
-                + " nom=" + nom + 
+        return "Personne{" +
+                "nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", nbVoitures=" + nbVoitures +
                 '}';
     }
+
+    // Getter pour le nom
+    public String getNom() {
+        return nom;
+    }
+
+    // Méthode pour ajouter une voiture
+    public void ajouterVoiture(Voiture voiture) {
+        if (nbVoitures  < 3) {
+            liste_voitures[nbVoitures ] = voiture;
+            nbVoitures ++;
+            voiture.setProprietaire(this); // Définir le propriétaire de la voiture
+        } else {
+            System.out.println("Limite de voitures atteinte.");
+        }
+    }
     
+    // Méthode pour obtenir la liste des voitures
+    public Voiture[] getListeVoitures() {
+        return liste_voitures;
+    }
 }
+
